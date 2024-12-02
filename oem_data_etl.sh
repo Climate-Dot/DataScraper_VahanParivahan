84565796e63f327a228cba28aff7a5403a498ff0
Monish Khambhati <monish@Monishs-MacBook-Air.local>
2024-12-01 21:52:24 -0800

    Enabling passing argument to bash script

--- oem_data_etl.sh
+++ oem_data_etl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Check if arguments (e.g., "OCT 2024") are passed
+ARGS="$@"
+
 # set up venv
 . /home/climate_dot_data/DataScraper_VahanParivahan/venv/bin/activate
 
@@ -7,16 +10,16 @@
 cd /home/climate_dot_data/DataScraper_VahanParivahan
 
 # Run the Extraction
-python3 OEM-level/oem_level_data_scraper.py
+python3 OEM-level/oem_level_data_scraper.py $ARGS
 
 # Run missing files extraction
-python3 OEM-level/get_missing_files.py
+python3 OEM-level/get_missing_files.py $ARGS
 
 # Run Pre Processing
-python3 OEM-level/data_preprocessing_v2.py
+python3 OEM-level/data_preprocessing_v2.py $ARGS
 
 # Ingestion
-python3 OEM-level/data_ingestion.py
+python3 OEM-level/data_ingestion.py $ARGS
 
 # File Upload and Cleanup
-python3 OEM-level/upload_files_to_blob_storage.py
+python3 OEM-level/upload_files_to_blob_storage.py $ARGS
--- state_ev_data_etl.sh
+++ state_ev_data_etl.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# set up venv
+# Check if arguments (e.g., "OCT 2024") are passed
+ARGS="$@"
+
+# Set up virtual environment
 . /home/climate_dot_data/DataScraper_VahanParivahan/venv/bin/activate
 
-# set up current directory
+# Set up current directory
 cd /home/climate_dot_data/DataScraper_VahanParivahan
 
 # Run the Extraction
-python3 State-level/state_level_data_scraper.py
+python3 State-level/state_level_data_scraper.py $ARGS
 
 # Run missing files extraction
-python3 State-level/state_level_get_missing_files.py
+python3 State-level/state_level_get_missing_files.py $ARGS
 
 # Run Pre Processing
-python3 State-level/state_level_data_pre_processing.py
+python3 State-level/state_level_data_pre_processing.py $ARGS
 
 # Ingestion
-python3 State-level/state_level_data_ingestion.py
+python3 State-level/state_level_data_ingestion.py $ARGS
 
 # File Upload and Cleanup
-python3 State-level/upload_files_to_blob_storage.py
+python3 State-level/upload_files_to_blob_storage.py $ARGS
