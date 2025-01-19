ab41ad27a742c3dba35e034760a0bc69b80c6566
Monish Khambhati <monish@Monishs-MacBook-Air.local>
2025-01-19 12:13:53 +0530

    reducing number of max worker

--- rto_level/rto_level_historical_data_scraper.py
+++ rto_level/rto_level_historical_data_scraper.py
@@ -224,9 +224,10 @@ def main():
                     # remove the file if already exists from previous month
                     if not os.path.exists(os.path.join(directory_path, "reportTable.xlsx")):
                         parameters.append((state, rto_office_name, year, month))
+
     # run selenium function in parallel
     with ThreadPoolExecutor(
-            max_workers=35
+            max_workers=30
     ) as executor:  # adjust max_workers based on your system's capability
         futures = [
             executor.submit(data_extract_class.run_selenium, args)
